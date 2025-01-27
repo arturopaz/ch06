c93fc02da7ba544283d919ccf7e3af3c35eca442
Arturo Paz <arturopazp@gmail.com>
2025-01-26 21:28:52 -0500

    agregar archivo user.py

---
+++ user.py
@@ -0,0 +1,11 @@
+user_0 = {
+    'username': 'apaz',
+    'first': 'arturo',
+    'last': 'paz',    
+}
+
+for key in user_0:
+    print(f"\nKey => {key}")
+    print(f"Value => {user_0[key]}")
+    # print(f"Value => {value}")
+
